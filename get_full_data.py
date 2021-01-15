507117e08199c88b0b1c271f91810e902f1dbc2b
PerceptronV <neutrinovs@gmail.com>
2021-01-15 15:44:38 +0800

    update repo struct

--- get_full_data.py
+++
@@ -1,5 +0,0 @@
-import dataloader
-from dataloader import get_save
-from datetime import datetime as dt
-
-get_save(dt(1970, 1, 1), dt(2020, 1, 1))
--- getname.py
+++
@@ -1,7 +0,0 @@
-from imfloader import get_dataset_struct
-
-key2des = get_dataset_struct('IFS')['Indicator']
-
-while 1:
-    i = input()
-    print(key2des[i]+'\n')
